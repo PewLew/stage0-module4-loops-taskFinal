7f5cb20bb2d834ccfc7c9d528028b4c2d09db3bb
Amachuarori <52317097+Amachuarori@users.noreply.github.com>
2023-12-18 15:45:49 +0600

    jn

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,10 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        int result = 1;
+        for(int i = 0; i<power; i++){
+            result *= numberToPrint;
+        }
     }
 
     public static void main(String[] args) {
